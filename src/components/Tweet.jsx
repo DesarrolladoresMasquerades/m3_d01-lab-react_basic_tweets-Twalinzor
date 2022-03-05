115d9b8db605b6e45e0022e4f8d92766eeae9e2f
Twalinzor <twalin.rift@gmail.com>
2022-03-05 18:57:50 +0100

    Lab

---
+++ src/components/Tweet.jsx
@@ -0,0 +1,27 @@
+import ProfileImage from "./ProfileImage";
+import User from "./User";
+import Timestamp from "./Timestamp";
+import Message from "./Message";
+import Actions from "./Actions";
+
+export default function Tweet(props) {
+  return (
+    <div className="tweet">
+      <ProfileImage imgSource={props.tweet.user.image} />
+
+      <div className="body">
+        <div className="top">
+          <User name={props.tweet.user.name} handle={props.tweet.user.handle} />
+
+          <Timestamp timestamp={props.tweet.timestamp} />
+        </div>
+
+        <Message message={props.tweet.message} />
+
+        <Actions />
+      </div>
+
+      <i class="fas fa-ellipsis-h"></i>
+    </div>
+  );
+}
